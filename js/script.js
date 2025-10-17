821c7245ec945a7b7e0d35d1c6025bfd01f7ebac
grudaa <gruda5959@gmail.com>
2025-10-17 18:08:55 +0200

    added functions for ui updating

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   const apiKey = CONFIG.WEATHER_API_KEY;
+  const textInput = document.getElementById("citySearch");
+  const temperature = document.getElementById("tempMain");
+  const humidityPerc = document.getElementById("humidity");
+  const windSpeed = document.getElementById("windSpeed");
+  const cityName = document.getElementById("cityName");
+
+  textInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      const city = getInput();
+      if (city.trim() !== "") {
+        getWeather(city);
+      }
+    }
+  });
 
   function getWeather(city) {
     //fetching api url and getting promise obj
@@ -8,21 +22,46 @@ document.addEventListener("DOMContentLoaded", () => {
     );
 
     fetchPromise
-    //when we get a response object back from the server, run the arrow function
       .then((Response) => {
+        //when we get a response object back from the server, run the arrow function
         if (!Response.ok) {
           throw new Error(`Http error: ${Response.status}`);
         }
         return Response.json(); //returns a new promise obj
+      })
+      .then((Data) => {
+        //starts when the promise is resolved
+        console.log(Data);
 
+        //update all data
+        updateTemp(Data.current.temp_c);
+        updateCity(Data.location.name);
+        updateHumidity(Data.current.humidity);
+        updateWind(Data.current.wind_kph);
       })
-      .then((Data) => { //starts when the promise is resolved 
-        console.log(Data); 
-      })
-      .catch((error) => { //starts when an error occurs in any part of the .then chain
-        console.error(`Fetch failed, ${error.message} occurred!`)
-      })
+      .catch((error) => {
+        //starts when an error occurs in any part of the .then chain
+        console.error(`Fetch failed, ${error.message} occurred!`);
+      });
   }
 
-  getWeather("Zagreb");
+  function updateTemp(temp) {
+    temperature.innerHTML = `${temp}°c`;
+  }
+
+  function updateHumidity(humidity) {
+    humidityPerc.innerHTML = `${humidity}%`;
+  }
+
+  function updateWind(wind) {
+    windSpeed.innerHTML = `${wind} km/h`;
+  }
+
+  function updateCity(city) {
+    cityName.innerHTML = city;
+  }
+
+  function getInput() {
+    return textInput.value;
+  }
 });
